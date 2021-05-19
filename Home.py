36e4ca114798f168d24f84748ebd2e97c0525da9
imdiode <nandanparagshukla@gmail.com>
2021-05-19 18:41:41 +0530

    with decimal done

--- Home.py
+++ Home.py
@@ -1,10 +1,23 @@
 import serial
-import time
 
 serialPort = serial.Serial('/dev/ttyUSB0', 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
 
-cmd = bytearray([130,130,82,0,0,0,84,0])
+gcmd = bytearray([130,130,82,0,0,0,84,0])
+dcmd = bytearray([130,130,82,12,0,0,84,12])
 
-serialPort.write(cmd)
+while True:
+    serialPort.write(gcmd)
+    res = serialPort.read(10)
+    pv = bytearray([res[0], res[1]])
+    sv = bytearray([res[2], res[3]])
+    pvint = int.from_bytes(pv, "little", signed="True")
+    svint = int.from_bytes(sv, "little", signed="True")
+    serialPort.write(dcmd)
+    res = serialPort.read(10)
+    decimal = bytearray([res[6], res[7]])
+    decimalint = int.from_bytes(decimal, "little", signed="True")
+    divs = pow(10, decimalint)
+    pvfl = pvint/pow(10, decimalint)
+    svfl = svint/pow(10, decimalint)
 
-res = serialPort.read(10)
+    print(str(pvfl) + ", " + str(svfl))
