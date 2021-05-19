f0ffe3d6df7c55578a98a869a8128bae0523876f
imdiode <nandanparagshukla@gmail.com>
2021-05-19 12:41:47 +0530

    Communication working

--- Home.py
+++ Home.py
@@ -1,2 +1,10 @@
 import serial
-serialPort = serial.Serial('COM3', 9600, timeout=1)
+import time
+
+serialPort = serial.Serial('/dev/ttyUSB0', 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+
+cmd = bytearray([130,130,82,0,0,0,84,0])
+
+serialPort.write(cmd)
+
+res = serialPort.read(10)
