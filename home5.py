588ffc6aa28ca14047e1e5ca2b8213f8ab709763
imdiode <nandanparagshukla@gmail.com>
2021-05-21 15:31:26 +0530

    implemented styling

--- home5.py
+++ home5.py
@@ -1,9 +1,14 @@
-import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import *
 
 app = QApplication([])
 app.setStyle('Fusion')
 window = QWidget()
+palette = QPalette()
+palette.setColor(QPalette.ButtonText, Qt.blue)
+app.setStyleSheet("QPushButton { margin: 10ex; background-color: #4747D2 }")
+app.setPalette(palette)
 layout = QVBoxLayout()
 layout.addWidget(QPushButton('Top'))
 layout.addWidget(QPushButton('Bottom'))
